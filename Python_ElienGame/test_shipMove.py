d0c152d3710673605a2a6ebdd1f9b1b22e8a4073
junsihuaite <2632173990@qq.com>
2022-06-03 08:47:49 +0800

    Tenth Commit

--- Python_ElienGame/test_shipMove.py
+++ Python_ElienGame/test_shipMove.py
@@ -9,18 +9,18 @@ from alien_invasion import AlienInvasion
 class TestShipMove(TestCase):
 
     def test_ship_moving_left(self):
-        """测试飞船左移"""
+        """飞船左移的测试"""
         ai = AlienInvasion()
         mock_g = Mock(return_value=ai)
         mock_obj = mock_g()
 
-        # 按左键
+        # 按下左键
         win32api.keybd_event(37, 0, 0, 0)
         # 更新游戏状态
         for i in range(100):
             mock_obj._check_events()
             mock_obj.ship.update()
-        # 判断按左键时moving_left是否为True
+        # 按下左键时标志是否为True
         self.assertEqual(mock_obj.ship.moving_left, True)
         # 判断飞船是否出界
         self.assertEqual(mock_obj.ship.rect.left >= 0, True)
@@ -29,22 +29,22 @@ class TestShipMove(TestCase):
         win32api.keybd_event(37, 0, win32con.KEYEVENTF_KEYUP, 0)
         mock_obj._check_events()
         mock_obj.ship.update()
-        # 判断松开左键后moving_left是否变为False
+        # 判断松开左键后标志是否变为False
         self.assertEqual(mock_obj.ship.moving_left, False)
 
     def test_ship_moving_right(self):
-        """测试飞船右移"""
+        """飞船右移的测试"""
         ai = AlienInvasion()
         mock_g = Mock(return_value=ai)
         mock_obj = mock_g()
 
-        # 按右键
+        # 按下右键
         win32api.keybd_event(39, 0, 0, 0)
         # 更新游戏状态
         for i in range(100):
             mock_obj._check_events()
             mock_obj.ship.update()
-        # 判断按右键时moving_right是否为True
+        # 按下右键时标志是否为True
         self.assertEqual(mock_obj.ship.moving_right, True)
         self.assertEqual(mock_obj.ship.rect.right <= mock_obj.screen.get_rect().right, True)
 
@@ -52,7 +52,7 @@ class TestShipMove(TestCase):
         win32api.keybd_event(39, 0, win32con.KEYEVENTF_KEYUP, 0)
         mock_obj._check_events()
         mock_obj.ship.update()
-        # 判断松开右键后moving_right是否变为False
+        # 判断松开右键后标志是否变为False
         self.assertEqual(mock_obj.ship.moving_right, False)
 
 
